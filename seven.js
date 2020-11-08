ae051fd023f85066e0eb199a16931d92916bcbfd
Limerio <44976231+Limerio@users.noreply.github.com>
2020-11-08 15:16:36 +0100

    Rich Presence Update

--- seven.js
+++ seven.js
@@ -1,25 +1,24 @@
-var rich = require("discord-rpc");
-var rpc = new rich.Client({transport:"ipc"});
+const rich = require("discord-rpc");
+const rpc = new rich.Client({ transport: "ipc" });
 const startTimestamp = new Date();
 
-var	clientId = "692341410820653076";
-
-async function setActivity() {
-rpc.setActivity({
-		details: "Team Anti-Raid",
-		state: "Bot Multifonction",
-		startTimestamp,
-		largeImageKey: 'seven_large',
-		largeImageText: 'Rejoignez-nous dès maintenant !',
-		smallImageKey: 'seven',
-		smallImageText: 'Vous ne serez pas déçu !',
-		instance: false
-	})
-}
+const clientId = "692341410820653076";
 	
 rpc.on('ready', () => {
-  setActivity();
-  console.log("Connecté");
+	try {
+		rpc.setActivity({
+			details: "Team Anti-Raid",
+			state: "Bot Multifonction",
+			startTimestamp,
+			largeImageKey: 'seven_large',
+			largeImageText: 'Rejoignez-nous dès maintenant !',
+			smallImageKey: 'seven',
+			smallImageText: 'Vous ne serez pas déçu !',
+			instance: false
+		})
+	} catch (e) {
+		return console.error(e)
+	}
 });
 
-rpc.login({clientId}).catch(console.error);
+rpc.login({ clientId }).then(() => console.log("Connecté")).catch(console.error);
